30404e1c08f42a5f127471934e126594178a4c85
Bartek <bartek.paczek@gmail.com>
2020-11-08 23:22:12 +0100

    zad2

--- lab2.cpp
+++ lab2.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+class Wektor
+{   public:
+    Wektor(int n)
+    {
+        double* value = new double[n];
+        *value = {0.};
+    };
+  
+    double* value;
+};
 
 int main()
 {
-    puts("dupa");
-}
+    Wektor dupa(5);
+    for(int i = 0;i++; 5)
+    {
+        std::cout<<dupa.value[i];
+    };
+};
