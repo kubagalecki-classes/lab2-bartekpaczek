1ac8840de4ee483d5e9ab9160090749ef64d8a0d
Bartek <bartek.paczek@gmail.com>
2020-11-11 17:53:53 +0100

    Zad11

--- lab2.cpp
+++ lab2.cpp
@@ -1,20 +1,84 @@
 #include <iostream>
 class Wektor
-{   public:
+{
+public:
     Wektor(int n)
     {
-        double* value = new double[n];
-        *value = {0.};
+        pojemnosc = 5;
+        dlugosc   = n;
+        value     = new double[n];
+        *value    = {0.};
     };
-  
+    
+
+    int  pojemnosc;
+    void setAll(double value_all)
+    {
+        for (int i = 0; i < this->dlugosc; i++) {
+            value[i] = value_all;
+        }
+    };
+
+    int get_dlugosc() { return dlugosc; };
+
+    int get_pojemnosc() { return pojemnosc; };
+
+    void print()
+    {
+        for (int i = 0; i < this->dlugosc; i++) {
+            std::cout << value[i] << "\n";
+        }
+    };
+    void zmienDlugosc(int n)
+    {
+        if (n <= pojemnosc) {
+            for (int i = n; i < this->pojemnosc; i++) {
+                value[i] = 0;
+            }
+        }
+        else {
+            double* temp;
+            temp  = new double[n];
+            *temp = {0.};
+            for (int i = 0; i < this->dlugosc; i++) {
+                temp[i] = value[i];
+            };
+            delete[] value;
+            value = new double[n];
+            value = temp;
+            delete[] temp;
+        };
+        dlugosc = n;
+    };
+    double& operator[](int ref)
+    {
+        if (ref>dlugosc)
+        {
+            zmienDlugosc(ref);
+        };
+        return value[ref];
+    };
+
+private:
+    int dlugosc;
     double* value;
 };
 
 int main()
 {
-    Wektor dupa(5);
-    for(int i = 0;i++; 5)
-    {
-        std::cout<<dupa.value[i];
-    };
-};
+    Wektor dupa(6);
+    dupa.setAll(2.);
+    dupa.print();
+    dupa.zmienDlugosc(10);
+    std::cout << "nowy1\n";
+    dupa.print();
+    std::cout << "nowy\n";
+    double& a = dupa[4];
+    a=12;
+    dupa.print();
+    std::cout << dupa.get_dlugosc();
+    std::cout << "nowy\n";
+    double& b = dupa[8];
+    b=12;
+    dupa.print();
+}
